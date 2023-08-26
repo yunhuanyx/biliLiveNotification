aefad6f1aa9b0b8e0685a31cbf3b9d76ac2daf45
yunhuanyx <95404593+yunhuanyx@users.noreply.github.com>
2023-08-26 16:40:54 +0800

    修改toast所使用库

--- 开播提醒.py
+++ 开播提醒.py
@@ -1,12 +1,17 @@
 import requests
 from retrying import retry
 import time
-from winotify import Notification, audio
+from win11toast import notify
+# from win10toast_click import ToastNotifier
+# from winotify import Notification, audio
+# import webbrowser
+# import json
 
 
-roomID = ["213"]
+# roomId = "213"
+roomID = ["213", "45104"]
 wait_time = 60
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 YaBrowser/19.7.0.1635 Yowser/2.5 Safari/537.36"}
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"}
 
 
 @retry(stop_max_attempt_number=5)
@@ -29,12 +34,11 @@ def main():
                     if live_status == 1:
                         if i == 0:
                             print("正常运行")
-                        toast = Notification(app_id=rid + "开播提醒",
-                                             title=rid + "已经开播了",
-                                             msg="已经开播了，点击打开直播间！ >>",
-                                             launch="https://live.bilibili.com/" + rid)
-                        toast.set_audio(audio.Default, loop=False)
-                        toast.show()
+                        notify(rid + "开播提醒",
+                               rid + "已经开播了",
+                               button={'activationType': 'protocol',
+                                       'arguments': "https://live.bilibili.com/" + rid,
+                                       'content': '打开直播间'})
                         roomID.remove(rid)
                         i += 1
                     else:
@@ -59,3 +63,34 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+'''
+def go():
+    webbrowser.open_new_tab("https://live.bilibili.com/"+roomId)
+
+
+
+def main():
+    print("监听中……")
+    url = "https://api.live.bilibili.com/room/v1/Room/room_init?id="+roomId
+    i = 0
+    toast = Notification(app_id="开播提醒",
+                         title="已经开播了",
+                         msg="已经开播了，点击打开直播间! >>",
+                         launch="https://live.bilibili.com/" + roomId)
+    toast.set_audio(audio.Default, loop=False)
+    while True:
+        if json.loads(requests.get(url).text)['data']['live_status'] == 1:
+            # ToastNotifier().show_toast("开播提醒", "已经开播了", threaded=True, callback_on_click=go)
+            toast.show()
+
+            break
+        else:
+            if i == 0:
+                print("当前未开播，继续监听……")
+            i += 1
+            time.sleep(60)
+
+
+'''
